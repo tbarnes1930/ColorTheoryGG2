00e3e05b6acceb6181d2a1c6f6fa15d9fb13a4d1
Barrington H. Scott <barrington_scott@bloomfield.edu>
2019-01-30 21:47:08 -0500

    Jumping Added!

---
+++ Assets/Scripts/Jumping.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumping : MonoBehaviour
+{
+
+
+    public Rigidbody rb;
+
+    public float upForce;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            MoveUp();
+        }
+
+    }
+
+    void MoveUp()
+    {
+        rb.AddForce(Vector3.up * upForce);
+        //rb.AddForce(transform.up * upForce);
+    }
+}
