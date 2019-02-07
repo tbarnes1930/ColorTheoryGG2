fb9c14ce8ad27636cf3ae709ef337d4937baacac
Barrington H. Scott <barrington_scott@bloomfield.edu>
2019-02-06 19:40:25 -0500

    Switches Functioning!

---
+++ Assets/Scripts/CreateObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateObject : MonoBehaviour
+{
+        public Transform Spawnpoint;
+        public GameObject Prefab;
+
+        void OnTriggerEnter()
+    {
+        Instantiate(Prefab, Spawnpoint.position, Spawnpoint.rotation);
+    }
+}
